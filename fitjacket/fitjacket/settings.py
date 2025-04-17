5ac9950a518df73c4fe293f4716e9aef51609f65
serrasencar <serrasencar@gmail.com>
2025-04-17 17:30:50 -0400

    Edit

--- fitjacket/fitjacket/settings.py
+++ fitjacket/fitjacket/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = "fitjacket.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://fitjacketdatabase_user:nGMPgm7ym8O0Np7fvLpJli4gHCvok74C@dpg-d00mqhqdbo4c73dg7170-a/fitjacketdatabase',
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
