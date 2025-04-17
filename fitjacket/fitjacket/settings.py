eda9f5ba6a388afe93c752a6b15b8fb989825c4d
serrasencar <serrasencar@gmail.com>
2025-04-17 17:19:30 -0400

    Update

--- fitjacket/fitjacket/settings.py
+++ fitjacket/fitjacket/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 
@@ -83,10 +84,11 @@ WSGI_APPLICATION = "fitjacket.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://fitjacketdatabase_user:nGMPgm7ym8O0Np7fvLpJli4gHCvok74C@dpg-d00mqhqdbo4c73dg7170-a/fitjacketdatabase',
+        conn_max_age=600
+    )
 }
 
 
